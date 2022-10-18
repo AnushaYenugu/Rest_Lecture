0e0a75e74494eb45c51328ce7619e07507cdfd40
Anusha Yenugu <anushayenugu92@gmail.com>
2022-10-18 08:04:47 +0200

    Initial Commit

---
+++ src/main/java/lexicon/spring/rest_lecture/RestLectureApplication.java
@@ -0,0 +1,13 @@
+package lexicon.spring.rest_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestLectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lexicon/spring/rest_lecture/RestLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package lexicon.spring.rest_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
